e73483d112d9d0b2264f4687a568ab23e4382b2f
asif@mev.ae <Mev@95096>
2018-06-27 17:54:51 +0400

    Updating sh new1. (06/26/2018, Asif)

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,5 @@ HockeyAppToken=ef4eaad18b6d46709739cffb3ae3be87
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=@Container0/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
 https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
