ed567b0394553b94615da60c5fa11bc051e91a5f
asif@mev.ae <Mev@95096>
2018-06-27 16:26:07 +0400

    Updating .sh(3) + yml(3). (06/26/2018, Asif)

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,6 @@ HockeyAppToken=b99ac9f308ae48d792e08279b0bf107e
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "notes_type=1" \
--F "notes=Merged in $CIRCLE_BRANCH branch, Commit URL: $CIRCLE_BUILD_URL" \
--F "repository_url=$CIRCLE_REPOSITORY_URL" \
--F "build_server_url=$CIRCLE_BUILD_URL" \
--F "commit_sha=$CIRCLE_SHA1" \
--F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
+-F "ipa=@CircleCIDemo2/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: $HockeyAppToken" \
 https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
