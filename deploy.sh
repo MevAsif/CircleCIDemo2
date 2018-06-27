9fd1676347f00e12a3d93e11d6fb81745d0a9124
asif@mev.ae <Mev@95096>
2018-06-27 17:43:38 +0400

    Updating sh. (06/26/2018, Asif)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 AndroidAppId=4e761043b5b648ff8d7c7345f8f3a8f8
-HockeyAppToken=b99ac9f308ae48d792e08279b0bf107e
+HockeyAppToken=ef4eaad18b6d46709739cffb3ae3be87
 
 curl \
 -F "status=2" \
