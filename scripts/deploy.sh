34717819dfad81dbdd7ca25f83afaae2f1dcba8e
asif@mev.ae <Mev@95096>
2018-06-27 12:44:20 +0400

    Adding deploy.sh file for Hockey app and updating xml. (06/26/2018, Asif)

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+AndroidAppId=4e761043b5b648ff8d7c7345f8f3a8f8
+HockeyAppToken=b99ac9f308ae48d792e08279b0bf107e
+
+curl \
+-F "status=2" \
+-F "notify=0" \
+-F "notes_type=1" \
+-F "notes=Merged in $CIRCLE_BRANCH branch, Commit URL: $CIRCLE_BUILD_URL" \
+-F "repository_url=$CIRCLE_REPOSITORY_URL" \
+-F "build_server_url=$CIRCLE_BUILD_URL" \
+-F "commit_sha=$CIRCLE_SHA1" \
+-F "ipa=@app/build/outputs/apk/debug/app-debug.apk" \
+-H "X-HockeyAppToken: $HockeyAppToken" \
+https://rink.hockeyapp.net/api/2/apps/$AndroidAppId/app_versions/upload
