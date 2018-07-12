f77dc330a2dabddfb5be6f92bdb195ee1d57d835
asif@mev.ae <Mev@95096>
2018-07-12 16:06:10 +0400

    Updating config xml.(Latest: version:40)

--- uploadseetest.sh
+++ uploadseetest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-SEETEST_IO_ACCESS_KEY=ef4eaad18b6d46709739cffb3ae3be87
+SEETEST_IO_ACCESS_KEY=eyJ4cC51IjoxNDUxNDUsInhwLnAiOjE0NTE0NCwieHAubSI6Ik1UVXpNRFl4TXpZd01ESTJOQSIsImFsZyI6IkhTMjU2In0.eyJleHAiOjE4NDY3MzQyMDgsImlzcyI6ImNvbS5leHBlcml0ZXN0In0.ymkmdTZ69rA3xwXbGigfNU8QQKmzwcV0qM--dY8aHko
 APP_PATH=file=@$PWD/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk
 curl -k -X POST https://demo.experitest.com/api/v1/applications/new -H "Authorization: Bearer $SEETEST_IO_ACCESS_KEY" -H "Cache-Control: no-cache" -H "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW" -F "ipa=@/home/circleci/project/outputs/outputs/apk/debug/app-debug.apk"
